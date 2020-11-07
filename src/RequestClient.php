136f31dd1869a0e567cd42997c9fadb93dc3b164
Tomas Palider (DJTommek) <tomas@palider.cz>
2020-11-07 23:02:02 +0100

    Added simple RequestClient making GET and POST requests compatible with Glympse API server

---
+++ src/RequestClient.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+
+namespace DJTommek\GlympseApi;
+
+class RequestClient
+{
+	const API_URL = 'https://api.glympse.com';
+
+	/**
+	 * @param string $path
+	 * @param array $queryParams
+	 * @param string|array<string,mixed> $body array for key-value request, string for raw body
+	 * @param string|null $authorization
+	 * @return mixed
+	 * @throws \JsonException
+	 */
+	public function makePostRequest(string $path, array $queryParams, $body = [], ?string $authorization = null)
+	{
+		if (is_array($body) === false && is_string($body) === false) {
+			throw new \InvalidArgumentException(sprintf('Parameter "$body" must be array or string but "%s" provided.', gettype($body)));
+		}
+		$url = sprintf('%s%s?%s', self::API_URL, $path, http_build_query($queryParams));
+		$headers = [
+			'Content-type: application/json',
+		];
+		if ($authorization) {
+			$headers[] = 'Authorization: Bearer ' . $authorization;
+		}
+		dump($body);
+		$response = Utils::fileGetContents($url, [
+			CURLOPT_POST => true,
+			CURLOPT_POSTFIELDS => $body,
+			CURLOPT_HTTPHEADER => $headers,
+		]);
+		$content = json_decode($response, false, 512, JSON_THROW_ON_ERROR);
+		if ($content->result === 'ok') {
+			return $content->response;
+		} else {
+			dump($content);
+			throw new \Exception($content->meta->error_detail ?? $content->meta->error);
+		}
+	}
+
+	public function makeGetRequest(string $path, array $queryParams, ?string $authorization = null)
+	{
+		$url = sprintf('%s%s?%s', self::API_URL, $path, http_build_query($queryParams));
+		$headers = [
+//			'Content-type: application/json',
+		];
+		if ($authorization) {
+			$headers[] = 'Authorization: Bearer ' . $authorization;
+		}
+		$response = Utils::fileGetContents($url, [
+			CURLOPT_HTTPHEADER => $headers,
+		]);
+		$content = json_decode($response, false, 512, JSON_THROW_ON_ERROR);
+		if ($content->result === 'ok') {
+			return $content->response;
+		} else {
+			throw new \Exception($content->meta->error_detail ?? $content->meta->error);
+		}
+	}
+}
